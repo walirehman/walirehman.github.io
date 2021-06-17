5d28d11b4dc4ddee0de22e038314d6251904e53a
walirehman <86072266+walirehman@users.noreply.github.com>
2021-06-17 09:57:12 -0700

    uploaded first fact finder

---
+++ factfinder/script.js
@@ -0,0 +1,34 @@
+document.querySelector('#f1').addEventListener("click", factOne);
+
+function factOne(){
+	document.querySelector('#b').innerHTML = "The Supreme Court has its own private basketball court with an amazing nickname. The highest court in the land.";
+	document.querySelector('#img').src= "images/basketball.jpeg";
+}
+
+document.querySelector('#f2').addEventListener("click", factTwo);
+
+function factTwo(){
+	document.querySelector('#b').innerHTML = "Vending machines are bigger threats to humanity than sharks.";
+	document.querySelector('#img').src= "";
+}
+
+document.querySelector('#f3').addEventListener("click", factThree);
+
+function factThree(){
+	document.querySelector('#b').innerHTML = "Americans have a troubling understanding of computer code.";
+	document.querySelector('#img').src= "";
+}
+
+document.querySelector('#f4').addEventListener("click", factFour);
+
+function factFour(){
+	document.querySelector('#b').innerHTML = "You can hire scary clowns to terrify children.";
+	document.querySelector('#img').src= "";
+}
+
+document.querySelector('#f5').addEventListener("click", factFive);
+
+function factFive(){
+	document.querySelector('#b').innerHTML = "The British royal family isn't allowed to play Monopoly.";
+	document.querySelector('#img').src= "";
+}
